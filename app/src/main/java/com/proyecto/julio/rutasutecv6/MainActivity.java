b67eec68e7b30363ba4878c0f047e10693f7ce86
julio <julioolivares90@gmail.com>
2017-04-23 20:25:32 -0600

    agregue un fragment para mostrar el mapa

--- app/src/main/java/com/proyecto/julio/rutasutecv6/MainActivity.java
+++ app/src/main/java/com/proyecto/julio/rutasutecv6/MainActivity.java
@@ -17,7 +17,7 @@ import android.view.Menu;
 import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity
-        implements NavigationView.OnNavigationItemSelectedListener,Inicio_fragment.OnFragmentInteractionListener {
+        implements NavigationView.OnNavigationItemSelectedListener,Inicio_fragment.OnFragmentInteractionListener,buscar_fragment.OnFragmentInteractionListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,6 +26,7 @@ public class MainActivity extends AppCompatActivity
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
---
+++ app/src/main/java/com/proyecto/julio/rutasutecv6/map_fragment.java
@@ -0,0 +1,74 @@
+package com.proyecto.julio.rutasutecv6;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapView;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.plus.model.people.Person;
+
+/**
+ * Created by julio on 04-23-17.
+ */
+
+public class map_fragment extends Fragment implements OnMapReadyCallback{
+    MapView mapView;
+    GoogleMap map;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
+        View view = inflater.inflate(R.layout.map_fragment,container,false);
+
+        //obtengo el mapa del xml
+        mapView = (MapView)view.findViewById(R.id.mapview);
+        mapView.onCreate(savedInstanceState);
+
+        //obtengo el mapa de mapview
+        mapView.getMapAsync(this);
+
+        return view;
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        LatLng latLng = new LatLng(13.6462418,-89.10387830000002);
+        float zoom = 13;
+        map=googleMap;
+        map.getUiSettings().setZoomControlsEnabled(true);
+        map.addMarker(new MarkerOptions().position(latLng));
+        map.moveCamera(CameraUpdateFactory.newLatLngZoom(latLng,zoom));
+    }
+    @Override
+    public void onResume(){
+        super.onResume();
+        mapView.onResume();
+    }
+    @Override
+    public void onPause(){
+        super.onPause();
+        mapView.onPause();
+    }
+    @Override
+    public void onDestroy(){
+        super.onDestroy();
+        mapView.onDestroy();
+    }
+    @Override
+    public void onSaveInstanceState(Bundle outState){
+        super.onSaveInstanceState(outState);
+        mapView.onSaveInstanceState(outState);
+    }
+    @Override
+    public void onLowMemory(){
+        super.onLowMemory();
+        mapView.onLowMemory();
+    }
+
+}
