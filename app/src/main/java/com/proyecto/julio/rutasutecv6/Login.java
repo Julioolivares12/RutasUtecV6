06efc7c5c99dda065d5685e66781f4163a482dc0
julio olivares <julioolivares90@gmail.com>
2017-05-04 21:28:36 -0600

    no se que cambie

--- app/src/main/java/com/proyecto/julio/rutasutecv6/Login.java
+++ app/src/main/java/com/proyecto/julio/rutasutecv6/Login.java
@@ -10,19 +10,13 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
-import DatosDB.RutasUtecDB;
-import DatosDB.usuarios;
-import Clogica.RutasUtecBD;
-import Clogica.Session;
-import DatosDB.RutasUtecDB;
+
 
 public class Login extends AppCompatActivity {
 
     private EditText txtusuario, txtpass;
     private Button btnLogin;
     private TextView txtregistro;
-    RutasUtecDB rutasUtecDB = new RutasUtecDB(this);
-    SQLiteDatabase db = rutasUtecDB.getWritableDatabase();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,21 +33,9 @@ public class Login extends AppCompatActivity {
             public void onClick(View v) {
                 String usuario = txtusuario.getText().toString();
                 String pass = txtpass.getText().toString();
-                int id=checkUsu(new usuarios(usuario,pass));
-                if (id==-1){
-                    Toast.makeText(getApplicationContext(),"usuario no existe",Toast.LENGTH_SHORT).show();
-                }
-                else
-                {
-                    Toast.makeText(getApplicationContext(),"usuario existe",Toast.LENGTH_SHORT).show();
-                    Intent intent = new Intent(getApplicationContext(),MainActivity.class);
-                    startActivity(intent);
-                }
-
             }
         });
-        rutasUtecDB.addusuario(new usuarios("julio","123"));
-        rutasUtecDB.addusuario(new usuarios("roberto","1234"));
+
 
         txtregistro.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -63,9 +45,7 @@ public class Login extends AppCompatActivity {
             }
         });
     }
-    public int checkUsu(usuarios usuarios){
-        return rutasUtecDB.checkuser(usuarios);
-    }
+   
 
 
 }
